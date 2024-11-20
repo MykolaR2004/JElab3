ec87aa6665be225db656599d155f283b332caafd
MykolaR2004 <mrize80blitz@gmail.com>
2024-11-20 10:07:10 +0200

    Deleted unused classes

--- src/main/java/org/example/jelab3/strategy/CalculationStrategy.java
+++
@@ -1,9 +0,0 @@
-package org.example.jelab3.strategy;
-
-import jakarta.servlet.http.HttpServletRequest;
-import org.example.jelab3.model.Result;
-
-public interface CalculationStrategy {
-    Result calculate(HttpServletRequest request);
-}
-
